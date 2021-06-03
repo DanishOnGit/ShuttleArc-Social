935c21074ae097157f410e1bbb2b09ba235e3234
DanishOnGit <youcandoit1911@gmail.com>
2021-06-03 18:58:15 +0530

    refactor:cleaned up unwanted code

---
+++ src/database/database.js
@@ -0,0 +1,10 @@
+export const colors = {
+    orange: {
+      900: "#db1d08",
+      700: "#fc452e",
+    },
+  };
+  export const fonts = {
+    heading: "Poppins",
+    body: "Poppins",
+  };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { colors, fonts } from "./database";
+import * as serviceWorker from "./serviceWorker";
+const theme = extendTheme({ colors, fonts });
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
