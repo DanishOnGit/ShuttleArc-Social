7dffe976d62ab7ce9a39e8f3bdddbd3defac43d5
DanishOnGit <youcandoit1911@gmail.com>
2021-07-19 02:19:33 +0530

    style:heading added on login page

--- src/features/login/Login.jsx
+++ src/features/login/Login.jsx
@@ -22,97 +22,106 @@ export const Login = () => {
 
   useEffect(() => initialRef.current.focus(), []);
   return (
-    <Flex width="full" align="center" justifyContent="center">
-      <Box p={5} boxShadow="2xl">
-        <Box textAlign="center">
-          <Heading>Login</Heading>
-        </Box>
-        <Box my={4} textAlign="left">
-          <FormControl isRequired>
-            <FormLabel>Email</FormLabel>
-            <Input
-              ref={initialRef}
-              value={userEmail}
-              onChange={(e) => setUserEmail(e.target.value)}
-              type="email"
-              placeholder="test@test.com"
-              focusBorderColor={colors.orange[500]}
-            />
-          </FormControl>
-          <FormControl mt={6} isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input
-              value={userPassword}
-              onChange={(e) => setUserPassword(e.target.value)}
-              type="password"
-              placeholder="*******"
-              focusBorderColor={colors.orange[500]}
-            />
-          </FormControl>
-          <Button
-            isLoading={status === "loading"}
-            onClick={async () => {
-              const result = await dispatch(
-                loginWithCredentials({ userEmail, userPassword })
-              );
-
-              toast({
-                title: `${
-                  result.payload.success ? "Logged In" : "Please try again"
-                }`,
-                description: `${result.payload.message}`,
-                status: `${result.payload.status}`,
-                duration: 2000,
-                isClosable: true,
-              });
-            }}
-            borderRadius="3rem"
-            bgColor={colors.orange[600]}
-            _hover={{ bgColor: colors.orange[700] }}
-            width="full"
-            mt={4}
-          >
-            Log In
-          </Button>
-        </Box>
-
-        <Text>
-          Don't have an account?{" "}
-          <Text color={colors.orange[700]} as="span">
-            <Link to="/signup">Signup</Link>
-          </Text>
-        </Text>
-        <Box>
-          <Text>-----------OR-----------</Text>
-        <Button
-            isLoading={status === "loading"}
-            onClick={async () => {
-              setUserEmail("danny@gmail.com");
-              setUserPassword("Danny@123");
-              const result = await dispatch(
-                loginWithCredentials({ userEmail:"danny@gmail.com", userPassword:"Danny@123" })
-              );
-
-              toast({
-                title: `${
-                  result.payload.success ? "Logged In" : "Please try again"
-                }`,
-                description: `${result.payload.message}`,
-                status: `${result.payload.status}`,
-                duration: 2000,
-                isClosable: true,
-              });
-            }}
-            borderRadius="3rem"
-            border="1px solid red"
-            _hover={{ bgColor: colors.orange[700] }}
-            width="full"
-            mt={4}
-          >
-            Use Test Credentials
-          </Button>
-        </Box>
+    <>
+      <Box textAlign="center" my={6}>
+        <Heading>Welcome to <Text as="span" color={colors.orange[600]}>Shuttlearc-Socials</Text></Heading>
       </Box>
-    </Flex>
+
+      <Flex width="full" align="center" justifyContent="center">
+        <Box p={5} boxShadow="2xl">
+          <Box textAlign="center">
+            <Heading>Login</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <FormControl isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input
+                ref={initialRef}
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+                type="email"
+                placeholder="test@test.com"
+                focusBorderColor={colors.orange[500]}
+              />
+            </FormControl>
+            <FormControl mt={6} isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input
+                value={userPassword}
+                onChange={(e) => setUserPassword(e.target.value)}
+                type="password"
+                placeholder="*******"
+                focusBorderColor={colors.orange[500]}
+              />
+            </FormControl>
+            <Button
+              isLoading={status === "loading"}
+              onClick={async () => {
+                const result = await dispatch(
+                  loginWithCredentials({ userEmail, userPassword })
+                );
+
+                toast({
+                  title: `${
+                    result.payload.success ? "Logged In" : "Please try again"
+                  }`,
+                  description: `${result.payload.message}`,
+                  status: `${result.payload.status}`,
+                  duration: 2000,
+                  isClosable: true,
+                });
+              }}
+              borderRadius="3rem"
+              bgColor={colors.orange[600]}
+              _hover={{ bgColor: colors.orange[700] }}
+              width="full"
+              mt={4}
+            >
+              Log In
+            </Button>
+          </Box>
+
+          <Text>
+            Don't have an account?{" "}
+            <Text color={colors.orange[700]} as="span">
+              <Link to="/signup">Signup</Link>
+            </Text>
+          </Text>
+          <Box>
+            <Text>-----------OR-----------</Text>
+            <Button
+              isLoading={status === "loading"}
+              onClick={async () => {
+                setUserEmail("danny@gmail.com");
+                setUserPassword("Danny@123");
+                const result = await dispatch(
+                  loginWithCredentials({
+                    userEmail: "danny@gmail.com",
+                    userPassword: "Danny@123",
+                  })
+                );
+
+                toast({
+                  title: `${
+                    result.payload.success ? "Logged In" : "Please try again"
+                  }`,
+                  description: `${result.payload.message}`,
+                  status: `${result.payload.status}`,
+                  duration: 2000,
+                  isClosable: true,
+                });
+              }}
+              borderRadius="3rem"
+              border="1px solid red"
+              _hover={{ bgColor: colors.orange[700] }}
+              width="full"
+              mt={4}
+            >
+              Use Test Credentials
+            </Button>
+          </Box>
+        </Box>
+      </Flex>
+    </>
   );
 };
